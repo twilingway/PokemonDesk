15bd21872253ac62f536bcaf51a03f77e53deb9a
GLAZUNOV ALEKSEI <993468@gmail.com>
2021-05-10 19:50:48 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './custom.css';
 import s from './App.modules.scss';
 
 const App = () => {
-    return ( <div className={cn(s.header, 'color')}>This is App Component!</div>)
+    return ( <div className={cn(s.header, s.color)}>This is App Component!</div>)
 }
 
 export default  App;
