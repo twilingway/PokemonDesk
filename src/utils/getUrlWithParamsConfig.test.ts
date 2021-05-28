5d9877f2463997dd397b9326f1a29601a1dd3b78
GLAZUNOV ALEKSEI <993468@gmail.com>
2021-05-28 16:26:14 +0400

    TDD

---
+++ src/utils/getUrlWithParamsConfig.test.ts
@@ -0,0 +1,33 @@
+import getUrlWithParamsConfig from './getUrlWithParamsConfig';
+
+describe('getUrlWithParamsConfig', () => {
+  test('Должна принимать два аргумента "getPokemons" и пустой объект, на выходе получить объект с полями pathname, protocol, host и пустым query', () => {
+    const url = getUrlWithParamsConfig('getPokemons', {});
+    expect(url).toEqual({
+      protocol: 'http',
+      host: 'zar.hosthot.ru',
+      pathname: '/api/v1/pokemons',
+      query: {},
+    });
+  });
+
+  test('Должна принимать два аргумента "getPokemons" и {name: "Pikachu"}, на выходе получить объект с полями pathname, protocol, host и query с полями name равное Pikachu', () => {
+    const url = getUrlWithParamsConfig('getPokemons', { name: 'Pikachu' });
+    expect(url).toEqual({
+      protocol: 'http',
+      host: 'zar.hosthot.ru',
+      pathname: '/api/v1/pokemons',
+      query: { name: 'Pikachu' },
+    });
+  });
+
+  test('Должна принимать два аргумента "getPokemonById" и {id: 25}, на выходе получить объект с полями pathname, protocol, host и пустой query', () => {
+    const url = getUrlWithParamsConfig('getPokemonById', { id: 25 });
+    expect(url).toEqual({
+      protocol: 'http',
+      host: 'zar.hosthot.ru',
+      pathname: '/api/v1/pokemon/25',
+      query: {},
+    });
+  });
+});
